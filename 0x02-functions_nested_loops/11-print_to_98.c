2a6b244c5986a179f25c129213fe202b1bc39360
Captainctech <captainc2015@gmail.com>
2022-07-06 08:57:39 -0700

    an update

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_to_98-print any number to 98
+* @n: number to start from
+* return: return to result
+*/
+void print_to_98(int n)
+{
+	upperband(n);
+	lowerband(n);
+	_putchar((98 / 10) + 48);
+	_putchar((98 % 10) + 48);
+	_putchar('\n');
+}
