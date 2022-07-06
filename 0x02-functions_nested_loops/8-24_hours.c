cca41174d490fd2af3e6f1d09b4920e53387fe78
Captainctech <captainc2015@gmail.com>
2022-07-06 08:32:54 -0700

    an update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,7 +10,7 @@ void jack_bauer(void)
 	int m1;
 	int m2;
 	int a = 9;
-	
+
 	h2 = 0;
 
 	while (h2 <= 2)
@@ -21,15 +21,12 @@ void jack_bauer(void)
 			a = 3;
 		}
 		h1 = 0;
-
 		while (h1 <= a)
 		{
 			m2 = 0;
-
 			while (m2 <= 5)
 			{
 				m1 = 0;
-
 				while (m1 <= 9)
 				{
 					_putchar('0' + h2);
