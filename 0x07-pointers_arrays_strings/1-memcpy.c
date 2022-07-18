f2dfba38033f8379742ffa8802e695f89ec2d475
Captainctech <captainc2015@gmail.com>
2022-07-18 05:46:56 -0700

    adding files

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
-* _memcpy - Copies @n bytes from the memory area pointed
-*           to by @src into that pointed to by @dest.
-* @dest: A pointer to the memory area to copy @src into.
-* @src: The source buffer to copy characters from.
-* @n: The number of bytes to copy from @src.
-* Return: A pointer to the destination buffer @dest.
+* _memcpy - copies memory area
+* @dest: destination of copy
+* @src: memory area to copy
+* @n: number of bytes to copy
+* Return: pointer to dest
 */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
