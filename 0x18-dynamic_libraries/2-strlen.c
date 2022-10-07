0b256fdf9c2dd4e6f1d8a9de602da8635e5aec9e
Captainctech <captainc2015@gmail.com>
2022-10-07 12:06:28 +0000

    adding files

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _strlen - Returns the length of a string.
+* @str: string.
+* Return: length.
+*/
+
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str++)
+	{
+		len++;
+	}
+	return (len);
+}
